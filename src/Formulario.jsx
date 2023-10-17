58b5a935384a8b3d22d7432fa16eafc98c6ffcae
Rodrigo Mata <rodry.mata@gmail.com>
2023-10-17 12:37:27 -0600

    Impresion en pantalla de las cripto monedas

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useSelectMonedas from "./Hooks/useSelectMonedas";
 import { monedas } from "./data/monedas";
 import styled from "@emotion/styled";
@@ -22,14 +22,24 @@ const InputSubmit = styled.input`
 `;
 
 const Formulario = () => {
-  const [moneda, SelectMonedas] = useSelectMonedas("Elige tu moneda", monedas); //Del archivo useSelectMonedas estamos exportando un arreglo es por eso que podemos poner en este array la palabra moneda en lugar de state
+  const [criptos,setCriptos] = useState('')
+  const [moneda, SelectMonedas] = useSelectMonedas("Elige tu Moneda", monedas); //Del archivo useSelectMonedas estamos exportando un arreglo es por eso que podemos poner en este array la palabra moneda en lugar de state
+  const [criptoMoneda, SelectCriptoMonedas] = useSelectMonedas("Elige tu Cripto Moneda", criptos); //Del archivo useSelectMonedas estamos exportando un arreglo es por eso que podemos poner en este array la palabra moneda en lugar de state
 
   useEffect(() => {
     const consultarAPI = async () => {
-      const url ="https://min-api.cryptocompare.com/data/top/mktcapfull?limit=30&tsym=USD"
+      const url ="https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD"
       const respuesta = await fetch(url)
       const resultado = await respuesta.json()
-      console.log(resultado.Data)
+      
+      const arrayCriptos = resultado.Data.map( cripto => { // cripto va a ser el que esta iterando
+        const objeto = { //Creando un objeto que contenga los siguientes apartados de la api que estamos trabajando
+          id: cripto.CoinInfo.Name,
+          nombre: cripto.CoinInfo.FullName,
+        }
+        return objeto
+      })
+      setCriptos(arrayCriptos)
     }
     consultarAPI()
   }, [])
@@ -37,6 +47,7 @@ const Formulario = () => {
   return (
     <form>
       <SelectMonedas />
+      <SelectCriptoMonedas />
 
       <InputSubmit type="submit" value="Cotizar" />
     </form>
